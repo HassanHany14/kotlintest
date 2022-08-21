1c69e93c36f7305824daaa199c416f2853e4281f
Hassan Hany <hassanemara321@gmail.com>
2022-08-21 17:45:07 +0200

    first code

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+
+
+fun main() {
+// print 90 degree rectangel
+     val rows = 5
+for (i in 0 until rows ) {
+    for (j in 0..i ){
+        print("*")
+    }
+    println(" ")
+}
+
+
+    }
+
+
+
